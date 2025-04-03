20523a56b047ff6460d784998bd3cedfa763c286
JulioRattlehead <juliocezar560@yahoo.com.br>
2025-04-03 19:27:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ discografia_megadeth = [
     {"album": "Countdown to Extinction", "ano": 1992, "destaque": "Symphony of Destruction", "musicas": ["Symphony of Destruction", "Sweating Bullets", "Skin o' My Teeth"]},
     {"album": "Peace Sells... but Who's Buying?", "ano": 1986, "destaque": "Peace Sells", "musicas": ["Peace Sells", "Wake Up Dead", "Black Friday"]},
     {"album": "Youthanasia", "ano": 1994, "destaque": "A Tout Le Monde", "musicas": ["A Tout Le Monde", "Train of Consequences", "Reckoning Day"]},
+    {"album": "Endgame", "ano": 2009, "destaque": "Head Crusher", "musicas": ["This Day We Fight! ", "44 Minutes", "EndGame"]},
+    {"album": "Thirdeen", "ano": 2011, "destaque": "FastLane", "musicas": ["New World Order", "Black Swan", "Fast Lane"]},
     {"album": "Dystopia", "ano": 2016, "destaque": "Dystopia", "musicas": ["Dystopia", "The Threat Is Real", "Fatal Illusion"]},
 ]
 
